f90f1dc5db6044faf8f9b5021bed528b73b6c705
st-user <nck.cd.339@gmail.com>
2021-01-28 18:13:12 +0900

    VRMサーバーからダウンロード時もprogress表示

--- components/VrmDataRequestHandler.js
+++ components/VrmDataRequestHandler.js
@@ -28,13 +28,26 @@ module.exports = class VrmDataRequestHandler extends RequestHandler {
         }
 
         const filePath = this.filePaths[vrmType];
-        const readStream = fs.createReadStream(filePath);
+        fs.stat(filePath, (error, stat) => {
+            if (error) {
+                res.writeHead(404, 'Not Found');
+                res.end();
+            }
+            res.writeHead(200, {
+                'Content-Type': 'application/octet-stream',
+                'Content-Length': stat.size
+            });
 
-        readStream.on('error', () => {
-            res.writeHead(404, 'Not Found');
-            res.end();
+            const readStream = fs.createReadStream(filePath);
+
+            readStream.on('error', () => {
+                res.writeHead(404, 'Not Found');
+                res.end();
+            });
+
+            readStream.pipe(res);
         });
 
-        readStream.pipe(res);
+
     }
 };
